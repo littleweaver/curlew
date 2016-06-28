2364aa1fc09818f1562197a71dc817ad9f4a2dba
Emily Horsman <me@emilyhorsman.com>
2016-06-27 21:42:59 -0400

    Test that App component renders a Navigator
    * Tests now pass

--- test/App.js
+++ test/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Navigator } from 'react-native'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 
 import App from '../components/App'
 
 describe('<App />', () => {
-	it('should render things', () => {
+	it('should render navigator', () => {
 		const wrapper = shallow(<App />)
-		expect(wrapper.find(View)).to.have.length(1)
-		expect(wrapper.find(Text)).to.have.length(2)
+		expect(wrapper.find(Navigator)).to.have.length(1)
 	})
 })
